d933df6044ec447e1e1ce4a2f82084660ac16d66
natthakit-glitch <natthakit2145@gmail.com>
2026-08-31 14:37:09 +0700

    update images

--- src/app/data/banddata.ts
+++ src/app/data/banddata.ts
@@ -1,9 +1,31 @@
 import { Band } from "@/types/band";
 
 export const bands: Band[] = [
-  {
-    
+  { 
     id: 1,
+    name: "Silly Fools",
+    genre: "Alternative Rock",
+    formedYear: 1997,
+    imageUrl: "/images/bands/sillyfools.jpg",
+    members: [
+      { name: "ปฏิเวธ อุทัยเฉลิม", role: "ร้องนำ" },
+      { name: "กมล สุโกศล", role: "กีตาร์" },
+      { name: "ธนดล จ๋วงพานิช", role: "กลอง" },
+    ],
+    },
+     { 
+    id: 2,
+    name: "Silly Fools",
+    genre: "Alternative Rock",
+    formedYear: 1997,
+    imageUrl: "/images/bands/sillyfools.jpg",
+    members: [
+      { name: "ปฏิเวธ อุทัยเฉลิม", role: "ร้องนำ" },
+      { name: "กมล สุโกศล", role: "กีตาร์" },
+      { name: "ธนดล จ๋วงพานิช", role: "กลอง" },
+    ],
+    }, { 
+    id: 3,
     name: "Silly Fools",
     genre: "Alternative Rock",
     formedYear: 1997,
